311ee07aa3ab43d3fe9b8ab52f0b753c60733c7f
jishan1019 <imranislamjishan200@gmail.com>
2023-12-07 13:35:45 +0600

    slice add and small device responsive fix

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,23 +1,25 @@
-import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import MyTasks from '../components/tasks/MyTasks';
-import TaskCard from '../components/tasks/TaskCard';
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import MyTasks from "../components/tasks/MyTasks";
+import TaskCard from "../components/tasks/TaskCard";
 
 const Tasks = () => {
   return (
-    <div className="h-screen grid grid-cols-12">
-      <div className="col-span-9 px-10 pt-10">
-        <div className="flex justify-between items-center">
+    <div className="h-screen grid grid-cols-1 lg:grid-cols-12">
+      <div className="col-span-9 px-2 lg:px-10 pt-10">
+        <div className="flex justify-between items-center ">
           <div>
-            <h1 className="font-semibold text-3xl">Tasks</h1>
+            <h1 className="font-semibold text-sm lg:text-3xl">Tasks</h1>
           </div>
-          <div className="flex gap-5">
+          <div className="flex gap-1 lg:gap-5">
             <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
               <MagnifyingGlassIcon className="h-6 w-6" />
             </button>
             <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
               <BellIcon className="h-6 w-6" />
             </button>
-            <button className="btn btn-primary">Add Task</button>
+            <button className="btn btn-primary text-xs lg:text-sm">
+              Add Task
+            </button>
             <div className="h-10 w-10 rounded-xl overflow-hidden">
               <img
                 src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
@@ -27,8 +29,8 @@ const Tasks = () => {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-3 gap-5 mt-10">
-          <div className="relative h-[800px] overflow-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-10">
+          <div className="relative lg:h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>Up Next</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
@@ -39,7 +41,8 @@ const Tasks = () => {
               <TaskCard />
             </div>
           </div>
-          <div className="relative h-[800px] overflow-auto">
+
+          <div className="relative lg:h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>In Progress</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
@@ -51,7 +54,8 @@ const Tasks = () => {
               <TaskCard />
             </div>
           </div>
-          <div className="relative h-[800px] overflow-auto">
+
+          <div className="relative lg:h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>Up Next</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
@@ -64,6 +68,7 @@ const Tasks = () => {
           </div>
         </div>
       </div>
+
       <div className="col-span-3 border-l-2 border-secondary/20 px-10 pt-10">
         <div>
           <h1 className="text-xl">Members</h1>
